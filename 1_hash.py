c06e675ceb3565f41bb1bc650f8883a8815cee09
Andrew Dong <andrewdong1994@gmail.com>
2021-05-12 15:12:46 -0400

    Add hash

---
+++ 1_hash.py
@@ -0,0 +1,14 @@
+# Define Dictionary
+thisdict = {
+    'andrew': 1234,
+    'avi': 123,
+    'tina': 345
+}
+
+# Get value by key
+x = thisdict['avi']
+
+# Set value by key
+thisdict['andrew'] = 9000
+
+# print all keys
