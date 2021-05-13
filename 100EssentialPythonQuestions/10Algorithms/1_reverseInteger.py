ae1e0039bcada179128456fb227f4af120bedbac
Andrew Dong <andrewdong1994@gmail.com>
2021-05-13 09:51:09 -0400

    Finished addstrings

--- 100EssentialPythonQuestions/10Algorithms/1_reverseInteger.py
+++ 100EssentialPythonQuestions/10Algorithms/1_reverseInteger.py
@@ -14,3 +14,26 @@ print(solution(-231))
 print(solution(345))
 
 # A warm-up algorithm, that will help you practicing your slicing skills. In effect the only tricky bit is to make sure you are taking into account the case when the integer is negative. I have seen this problem presented in many different ways but it usually is the starting point for more complex requests.
+
+# 1. Slicing
+stringname[stringlength::-1]
+stringname[::-1]
+
+s = "Python"
+stringlength = len(s)
+slicedString = s[stringlength::-1]
+print(slicedString)
+
+# 2. Loop
+s = "Python"
+reversedString = []
+index = len(s)
+while index > 0:
+    reversedString += s[index - 1]
+    index -= 1
+print(reversedString)
+
+# 3 Use join
+s = "Python"
+reversed = ''.join(reversed(s))
+print(reversed)
--- 100EssentialPythonQuestions/10Algorithms/2_averageWordsLength.py
+++ 100EssentialPythonQuestions/10Algorithms/2_averageWordsLength.py
@@ -6,7 +6,20 @@ sentence2 = "I need to work very hard to learn more about algorithms in Python!"
 
 
 def solution(sentence):
+    # iterate through the symbols
     for p in "!?',;.":
+        # string.replace(oldvalue, newvalue, count)
         sentence = sentence.replace(p, '')
+    # string.split(separator, maxsplit)
     words = sentence.split()
+    # round(number, digits)
+    # sum(iterable, start)
+    # len(object)
+    # for item in iterable
     return round(sum(len(word) for word in words/len(words), 2))
+
+
+print(solution(sentence1))
+print(solution(sentence2))
+
+# Algorithms that require you to apply some simple calculations using strings are very common, therefore it is important to get familiar with methods like .replace() and .split()that in this case helped me removing the unwanted characters and create a list of words, the length of which can be easily measured and summed.
